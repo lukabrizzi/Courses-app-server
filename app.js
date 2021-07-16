97aec34fb5c018cf28ed8dafd57cc3728ef11865
Luka Brizzi <lukabrizzi@MacBook-Air-de-Luka.local>
2021-07-15 23:17:40 -0300

    104

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const { API_VERSION } = require('./config');
+
+// Load routings
+const authRoutes = require('./routers/auth');
+const userRoutes = require("./routers/user");
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(cors({ origin: true, credentials: true }));
+
+// Config header HTTP
+// ...
+
+//Router basic
+app.use(`/api/${API_VERSION}`, authRoutes);
+app.use(`/api/${API_VERSION}`, userRoutes);
+
+
+module.exports = app;
---
+++ controllers/user.js
@@ -0,0 +1,105 @@
+const bcrypt = require("bcrypt-nodejs");
+const jwt = require("../services/jwt");
+const User = require("../models/user");
+
+function signUp(req, res) {
+    const user = new User();
+
+    const { name, lastname, email, password, verifPassword } = req.body;
+
+    user.name = name;
+    user.lastname = lastname;
+    user.email = email.toLowerCase();
+    user.role = "admin";
+    user.active = false;
+
+
+    if (!password || !verifPassword) {
+        res.status(404).send({ message: "Debe completar las dos contraseñas." });
+    } else {
+        if (password !== verifPassword) {
+            res.status(404).send({ message: "Las contraseñas deben ser iguales." });
+        } else {
+            bcrypt.hash(password, null, null, function (err, hash) {
+                if (err) {
+                    res.status(500).send({ message: "Error al encriptar la contraseña." })
+                } else {
+                    user.password = hash;
+
+                    user.save((err, userStored) => {
+                        if (err) {
+                            res.status(500).send({ message: "El usuario ya existe." });
+                        } else {
+                            if (!userStored) {
+                                res.status(404).send({ message: "Error al crear el usuario." });
+                            } else {
+                                res.status(200).send({ user: userStored });
+                            }
+                        }
+                    });
+                }
+            })
+        }
+    }
+}
+
+function signIn(req, res) {
+    const params = req.body;
+    const email = params.email.toLowerCase();
+    const password = params.password;
+
+    User.findOne({ email }, (err, userStored) => {
+        if (err) {
+            res.status(500).send({ message: "Error del servidor" })
+        }
+
+        if (!userStored) {
+            res.status(404).send({ message: "Usuario no encontrado" })
+        } else {
+            bcrypt.compare(password, userStored.password, (err, check) => {
+                if (err) {
+                    res.status(500).send({ message: "Error del servidor" })
+                } else if (!check) {
+                    res.status(500).send({ message: "La contraseña es incorrecta" })
+                } else {
+                    if (!userStored.active) {
+                        res.status(200).send({ code: 200, message: "El usuario no se ha activado" })
+                    } else {
+                        res.status(200).send({
+                            accessToken: jwt.createAccessToken(userStored),
+                            refreshToken: jwt.createRefreshToken(userStored)
+                        })
+                    }
+                }
+            })
+        }
+    })
+}
+
+function getUsers(req, res) {
+    User.find().then(users => {
+        if (!users) {
+            res.status(404).send({ message: "No se ha encontrado ningun usuario." })
+        } else {
+            res.status(200).send({ users });
+        }
+    })
+}
+
+function getUsersActive(req, res) {
+    const query = req.query;
+    User.find({ active: query.active }).then(users => {
+        if (!users) {
+            res.status(404).send({ message: "No se ha encontrado ningun usuario activo." })
+        } else {
+            res.status(200).send({ users });
+        }
+    })
+}
+
+module.exports = {
+    signUp,
+    signIn,
+    getUsers,
+    getUsersActive
+}
